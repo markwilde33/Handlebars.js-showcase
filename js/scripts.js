049bd6760edb1b0be747bdac9290bc9116d4ae9c
markwilde33 <markwilde29@gmail.com>
2019-09-11 12:21:35 +0100

    Added two more character objects. Added a new property -- favorite-color; added an if else statement so that a placeholder of Secret will display in the browser if it is not included in the character object.

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,61 @@
 var cast = {
 "characters": [
-{
-"name": "Jon Snow",
-"shortCode": "jon-snow",
-"actor": "Kit Harrington",
-"house": "Stark",
-"location": "The Wall"
-},
+
 {
 "name": "Tyrion Lannister",
 "shortCode": "tyrion",
 "actor": "Peter Dinklage",
 "house": "Lannister",
-"location": "Kings Landing"
+"location": "Kings Landing",
+"favorite-color": "Purple"
 },
 {
 "name": "Brienne of Tarth",
 "shortCode": "brienne",
 "actor": "Gwendoline Christie",
 "house": "Tarth",
-"location": "The Twins"
+"location": "The Twins",
+"favorite-color": ""
 },
 {
 "name": "Eddard Stark",
 "shortCode": "ned-stark",
 "actor": "Sean Bean",
 "house": "Stark",
-"location": " Crypt of Winterfell"
+"location": "Crypt of Winterfell",
+"favorite-color": "Yellow"
+},
+{
+"name": "Cersei",
+"shortCode": "cersei",
+"actor": "Lena Headley",
+"house": "Stark",
+"location": "Kings Landing",
+"favorite-color": "Blood Red"
+},
+{
+"name": "Jon Snow",
+"shortCode": "jon-snow",
+"actor": "Kit Harrington",
+"house": "Stark",
+"location": "The Wall",
+"favorite-color": "Marine Green"
+},
+{
+"name": "Daenarys Targaryen",
+"shortCode": "daenerys",
+"actor": "Emilia Clarke",
+"house": "Targaryen",
+"location": "Mereen",
+"favorite-color": "Fiery Orange"
 },
 {
 "name": "Sandor Clegane",
 "shortCode": "the-hound",
 "actor": "Rory McCann",
 "house": "Clegane",
-"location": "Looking for chickens.."
+"location": "Looking for chickens..",
+"favorite-color": ""
 }
 ]
 }
